a3c14ae03b39c3f284cd5a82edda4fb971d12dad
toseth97 <oluwasheun9721@gmail.com>
2023-05-06 14:55:02 +0100

    adding z index to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     let date = new Date().getFullYear();
     return (
-        <footer className="w-full sticky bottom-0 dark:bg-gray-900 px-16 dark:text-white dark:text-white flex items-center flex-col py-2">
+        <footer className="w-full sticky bottom-0 bg-white text-black z-10 dark:bg-gray-900 px-16 dark:text-white dark:text-white flex items-center flex-col py-2">
             <h1 className="mt-4">S.PERSIS</h1>
             <div className="w-full md:flex items-center justify-between text-center">
                 <div>
