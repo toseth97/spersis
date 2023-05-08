d5df14ecf2f2ed4ab36f6e438079a89362232925
toseth97 <oluwasheun9721@gmail.com>
2023-05-08 15:14:00 +0100

    quick fix on experience css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,23 +2,23 @@ import React from "react";
 
 const Navigation = () => {
     return (
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-10">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" class="flex items-center">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-10">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="/" className="flex items-center">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                         SPersis
                     </span>
                 </a>
                 <button
                     data-collapse-toggle="navbar-default"
                     type="button"
-                    class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-default"
                     aria-expanded="false"
                 >
-                    <span class="sr-only">Open main menu</span>
+                    <span className="sr-only">Open main menu</span>
                     <svg
-                        class="w-6 h-6"
+                        className="w-6 h-6"
                         aria-hidden="true"
                         fill="currentColor"
                         viewBox="0 0 20 20"
@@ -32,14 +32,14 @@ const Navigation = () => {
                     </svg>
                 </button>
                 <div
-                    class="hidden w-full md:block md:w-auto"
+                    className="hidden w-full md:block md:w-auto"
                     id="navbar-default"
                 >
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a
                                 href="#home"
-                                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                                 aria-current="page"
                             >
                                 Home
@@ -48,7 +48,7 @@ const Navigation = () => {
                         <li>
                             <a
                                 href="#about"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 About
                             </a>
@@ -56,7 +56,7 @@ const Navigation = () => {
                         <li>
                             <a
                                 href="#service"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 Services
                             </a>
@@ -64,7 +64,7 @@ const Navigation = () => {
                         <li>
                             <a
                                 href="#experience"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                             >
                                 Experience
                             </a>
@@ -73,7 +73,7 @@ const Navigation = () => {
                             <button className="">
                                 <a
                                     href="mailto:samuelpersisoladejo@gmail.com"
-                                    class="block w-full py-1 pl-3 px-4 pr-4 text-white hover:bg-blue-700 bg-blue-500 rounded md:border-0"
+                                    className="block w-full py-1 pl-3 px-4 pr-4 text-white hover:bg-blue-700 bg-blue-500 rounded md:border-0"
                                 >
                                     Hire me...
                                 </a>
